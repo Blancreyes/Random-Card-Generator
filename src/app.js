f01be1e20e5193c2e3a4732ee4f9f4fce7bb79d4
Blancreyes <luijbr83@gmail.com>
2022-11-01 17:38:18 +0000

    RandomGenerated

--- src/app.js
+++ src/app.js
@@ -2,11 +2,31 @@
 import "./style.css";
 
 window.onload = () => {
-  document.querySelector(".card").classList.add("spade");
-  setTimeout(() => {
-    document.querySelector(".card").classList.remove("spade");
-    document.querySelector(".card").classList.add("heart");
-  }, 1000);
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
 };
 
-let generatera;
+let generateRandomNumber = () => {
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * number.length);
+  return number[indexNumbers];
+};
+let generateRandomSuit = () => {
+  let suit = ["diamond", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
